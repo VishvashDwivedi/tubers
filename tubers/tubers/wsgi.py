9b29cf6cdfd8e38ccaa271643f28612feb972ddd
Vishvash Dwivedi <vishvashdwivedi@Vishvashs-MacBook-Pro.local>
2022-01-31 14:33:48 +0530

    Second Commit

--- tubers/tubers/wsgi.py
+++ tubers/tubers/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tubers.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tubers.tubers.settings')
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tubers.settings')
 
 application = get_wsgi_application()
